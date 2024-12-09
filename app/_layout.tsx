3d64da06f83ef2a1d375faa3ac4df0889c4e491c
Carlos Veizaga <carlosveizaga.jcvc@gmail.com>
2024-12-09 10:27:43 -0400

    task: Indicator implemented and tanstack was integrated.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import "../src/assets/css/global.css";
 import { nowPlayingAction } from '@/src/core/actions/movies/now-playing.action';
+import { Stack } from 'expo-router';
 
 // Create a client
 const queryClient = new QueryClient()
@@ -12,9 +12,7 @@ const queryClient = new QueryClient()
 const RootLayout = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <View>
-        <Text className='text-3xl'>RootLayout</Text>
-      </View>
+      <Stack screenOptions={{ headerShown: false }}></Stack>
     </QueryClientProvider>
   )
 }
---
+++ app/home/index.tsx
@@ -0,0 +1,26 @@
+import { View, Text, ActivityIndicator } from 'react-native'
+import React from 'react'
+import { useMovies } from '@/src/presentation/hooks/useMovies'
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
+
+const HomeScreen = () => {
+    const safeArea = useSafeAreaInsets()
+    const { nowPlayingQuery } = useMovies()
+
+    if (nowPlayingQuery.isLoading) {
+        return (
+            <View className='justify-center items-center flex-1'>
+                <ActivityIndicator color="purple" size={40} />
+            </View>
+        )
+    }
+
+    return (
+        <View className='mt-2' style={{paddingTop: safeArea.top}}>
+            <Text className='text-3xl font-bold px-4 mb-2'>HomeScreen</Text>
+            <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
